b544109b1d24bf1fe95e3136fea46e336e0ee99d
Danila Petrov <daniilpetrov776@gmail.com>
2024-06-02 22:07:34 +0300

    Последние функции

--- js/functions.js
+++ js/functions.js
@@ -1,21 +1,3 @@
-// Два варианта функции проверки Палиндрома
-
-// function isPalindrome (string) {
-//   const formattedString = string
-//     .toLowerCase()
-//     .replaceAll(' ', '');
-//   const reversedString = formattedString
-//     .split('')
-//     .reverse()
-//     .join('');
-//   for (let i = 0; i < formattedString.length; i++) {
-//     if (formattedString.at(i) !== reversedString.at(i)) {
-//       return false
-//       }
-//     }
-//     return true
-//  }
-
 function isPalindrome (string) {
   const formattedString = string
     .toLowerCase()
@@ -27,21 +9,37 @@ function isPalindrome (string) {
   return formattedString === reversedString;
 }
 
-// console.log(isPalindrome('Лёша на полке клопа нашёл '))
+isPalindrome('Лёша на полке клопа нашёл ');
 
-
-function getNumber (string) {
-  const number = Math.abs(string);
-  if (!isNaN(number)) {
-    return number;
-  }
+function getNumber (i) {
+  const string = String(i);
   const filter = string.match(/[0-9]/gi);
   if (filter === null) {
     return NaN;
   } const result = +filter.join('');
   return result;
-
-
 }
-console.log(getNumber('wqe21'))
 
+getNumber('1.5');
+
+function addSymbol(initial, length, add) {
+  if (length - initial.length - add.length === 1) {
+    const newString = initial.padStart(length - initial.length, add);
+    const cutString = add.slice(0, length - add.length - 1);
+    const wholeString = cutString + newString;
+    return wholeString;
+  } {
+    const newString = initial.padStart(length, add);
+    return newString;
+  }
+}
+
+addSymbol('q', 4,'we');
+
+function checkLength (text, length) {
+  const string = String(text);
+  if (string.length <= length) {
+    return true;
+  } return false;
+}
+checkLength('Проверка длинны строк', 10);
