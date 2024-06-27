472a21ba1fdd5a8a01244879c952879ea12e9824
Danila Petrov <daniilpetrov776@gmail.com>
2024-06-27 15:47:43 +0300

    Связал данные и картинку

--- js/fullphoto.js
+++ js/fullphoto.js
@@ -1,4 +1,6 @@
 import { isEscapeKey, isEnterKey } from './util.js';
+import {photos } from './main.js';
+
 const fullphoto = document.querySelector('.overlay');
 const body = document.querySelector('body');
 const commentCount = fullphoto.querySelector('.social__comment-count');
@@ -30,9 +32,41 @@ const closeFullPhoto = () => {
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
+const matchFullPhoto = (evt) => {
+  const photo = photos.find((item) => item.id === Number(evt.target.closest('.picture').dataset.id));
+  const bigImageContainer = fullphoto.querySelector('.big-picture__img');
+  const bigImage = bigImageContainer.querySelector('img');
+  const likesCount = fullphoto.querySelector('.likes-count');
+  const comments = fullphoto.querySelector('.comments-count');
+  const socialCaption = fullphoto.querySelector('.social__caption');
+
+  bigImage.src = photo.url;
+  likesCount.textContent = photo.likes;
+  comments.textContent = photo.comments.length;
+  socialCaption.textContent = photo.description;
+
+  const commentsContainer = fullphoto.querySelector('.social__comments');
+  const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+  const fragment = document.createDocumentFragment();
+
+  photo.comments.forEach(({avatar,name ,message }) => {
+    const commentTemplateClone = commentTemplate.cloneNode(true);
+    const commentAvatar = commentTemplateClone.querySelector('.social__picture');
+    const commentText = commentTemplateClone.querySelector('.social__text');
+
+    commentAvatar.src = `img/avatar-${avatar}.svg` ;
+    commentAvatar.alt = name;
+    commentText.textContent = message;
+    fragment.appendChild(commentTemplateClone);
+  });
+  commentsContainer.appendChild(fragment);
+};
+
 picturesContainer.addEventListener('click', (evt) => {
-  if (evt.target.matches('img')) {
+  if (evt.target.closest('.picture')) {
     getFullPhoto();
+    matchFullPhoto(evt);
+    // console.log(evt.target);
   }
 });
 picturesContainer.addEventListener('keydown', (evt) => {
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import { getPhotos } from './data.js';
 import { renderPhotos } from './photos.js';
 import './fullphoto.js';
-import './linkphotos.js';
 
 const photos = getPhotos();
 
 renderPhotos(photos);
 
+export {photos};
