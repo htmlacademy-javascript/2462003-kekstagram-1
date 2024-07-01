70c4cc6e730de6f76866d39e718eaec47aa03be9
Danila Petrov <daniilpetrov776@gmail.com>
2024-07-01 21:09:29 +0300

    Первое открытие

--- js/full-photo.js
+++ js/full-photo.js
@@ -8,9 +8,10 @@ const commentsContainer = fullPhoto.querySelector('.social__comments');
 const bigImageContainer = fullPhoto.querySelector('.big-picture__img');
 const bigImage = bigImageContainer.querySelector('img');
 const likesCount = fullPhoto.querySelector('.likes-count');
-const commentsCount = fullPhoto.querySelector('.comments-count');
+// const commentsCount = fullPhoto.querySelector('.comments-count');
 const socialCaption = fullPhoto.querySelector('.social__caption');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
+let firstComment = 0;
 let lastComment = 5;
 
 const onDocumentKeydown = (evt) => {
@@ -39,34 +40,43 @@ const getComment = ({avatar, name, message}) => {
 };
 
 const renderComments = (comments) => {
+  if (lastComment >= comments.length) {
+    commentLoader.classList.add('hidden');
+    lastComment = comments.length;
+  }
   const fragment = document.createDocumentFragment();
-  const commentsToRender = comments.slice(0,lastComment);
-  console.log(commentsToRender);
+  const commentsToRender = comments.slice(firstComment, lastComment);
 
   commentsToRender.forEach((comment) => {
     fragment.appendChild(getComment(comment));
   });
   commentsContainer.appendChild(fragment);
+  commentCount.innerHTML = `${lastComment} из ${comments.length} комментариев`;
+  const onCommentLoaderClick = () => {
+    lastComment = lastComment + 5;
+    firstComment = firstComment + 5;
 
-  // const commentsContainerContent = commentsContainer.children;
+    renderComments(comments);
+  };
+
+  commentLoader.addEventListener('click', onCommentLoaderClick);
 };
 
-
-const matchFullPhoto = (photo) => {
+function matchFullPhoto (photo) {
   bigImage.src = photo.url;
   likesCount.textContent = photo.likes;
-  // commentsCount.textContent = photo.comments.length;
   socialCaption.textContent = photo.description;
   renderComments(photo.comments);
-  commentCount.innerHTML = `${lastComment} из ${commentsCount.textContent = photo.comments.length} комментариев`;
-  console.log(commentsContainer.children);
-};
-
+  // commentCount.innerHTML = `${lastComment} из ${photo.comments.length} комментариев`;
+}
 
 const openFullPhoto = (photo) => {
   fullPhoto.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
+  commentLoader.classList.remove('hidden');
+  firstComment = 0;
+  lastComment = 5;
   matchFullPhoto(photo);
 };
 
@@ -75,11 +85,5 @@ const onCloseButtonClick = () => {
 };
 
 closeButton.addEventListener('click', onCloseButtonClick);
-
-const onCommentLoaderClick = () => {
-  lastComment = lastComment + 5;
-};
-
-commentLoader.addEventListener('click', onCommentLoaderClick);
 
 export {openFullPhoto};
